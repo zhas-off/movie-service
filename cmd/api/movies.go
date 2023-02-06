1ff70c94cccd296282f1ccf3af2c07354ad8ddaa
Zhasulan <jasik_2000@mail.ru>
2023-02-06 11:57:47 +0600

    Add GET movies and POST movies/:id endpoints

---
+++ cmd/api/movies.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// createMovieHandler handles "POST /v1/movies" endpoint.
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+// showMovieHandler handles "Get /v1/movies/:id" endpoint. For now, it returns plain-text
+// placeholder response using the interpolated "id" parameter from the current URL
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// When httprouter is parsing a request, any interpolated URL Parameters will be stored
+	// in the request context. We can use the ParamsFromContexnt() function to retrieve a slice
+	// containing these paremter names and values.
+
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Otherwise, interpolate the movie ID in a placeholder response.
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
