f1e70221b27697a158d4b772cf08cf496f648952
Zhasulan <jasik_2000@mail.ru>
2023-02-06 12:15:19 +0600

    Adding custom movie data

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/zhas-off/movie-service/internal/data"
 )
 
 // createMovieHandler handles "POST /v1/movies" endpoint.
@@ -24,5 +27,23 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	// Otherwise, interpolate the movie ID in a placeholder response.
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	// Create a new instance of the Movie struct, containing the ID we extracted from the URL and
+	// some dummy data. Also notice that we deliberately haven't set a value for the Year field.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounterd a problem and could not process your request",
+			http.StatusInternalServerError)
+		return
+	}
 }
