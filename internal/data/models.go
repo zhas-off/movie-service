fbe78bd261bc9d7f7de1ac813444aedda535b661
Zhasulan <jasik_2000@mail.ru>
2023-02-27 23:51:54 +0600

    User activation

--- internal/data/models.go
+++ internal/data/models.go
@@ -3,8 +3,6 @@ package data
 import (
 	"database/sql"
 	"errors"
-	"log"
-	"os"
 )
 
 var (
@@ -19,21 +17,13 @@ var (
 type Models struct {
 	Movies MovieModel
 	Users  UserModel
+	Tokens TokenModel
 }
 
 func NewModels(db *sql.DB) Models {
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	return Models{
-		Movies: MovieModel{
-			DB:       db,
-			InfoLog:  infoLog,
-			ErrorLog: errorLog,
-		},
-		Users: UserModel{
-			DB:       db,
-			InfoLog:  infoLog,
-			ErrorLog: errorLog,
-		},
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
